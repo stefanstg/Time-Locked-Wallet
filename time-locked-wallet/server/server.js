f88097342a6c43a9b61e8032e416045567c3f4e1
dianavintila <58642790+dianavintila@users.noreply.github.com>
2023-01-03 17:03:10 +0200

    Angular  project

--- time-locked-wallet/server/server.js
+++
@@ -1,8 +0,0 @@
-const express = require('express')
-const app = express()
-
-app.get("/api", (req, res) => {
-  res.json({"users": ["user1", "user2" ]})
-})
-
-app.listen(3001, () => {console.log("Server started on port 3001")})
